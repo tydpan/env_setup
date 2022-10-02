ed966e835a1572550e482008fcb8be26493825f8
Tai-Yu Pan <daniel@typ.local>
2022-10-02 01:07:51 -0400

    update

--- zsh/install_omz.sh
+++ zsh/install_omz.sh
@@ -1,13 +1,12 @@
 version=$( zsh --version )
 if [ $? -ne 0 ]; then
     # for linux becuase zsh has became default shell in new OS
-    sudo apt install zsh
+    sudo apt install -y zsh
     chsh -s $(which zsh) # set to default shell
     version=$( zsh --version )
 fi
 echo "$version"
 
 # Oh-My-Zsh
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sed "s/ZSH_THEME=.*/ZSH_THEME=\"clean\"/" $HOME/.zshrc # my theme
-
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+sed -i "s/ZSH_THEME=.*/ZSH_THEME=\"clean\"/" $HOME/.zshrc # my theme
